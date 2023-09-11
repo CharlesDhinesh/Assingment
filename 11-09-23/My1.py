61ba486095240ec300ac92e9d3ee1793f8896f30
CharlesDhinesh <144647044+CharlesDhinesh@users.noreply.github.com>
2023-09-11 11:55:48 +0530

    Add files via upload

---
+++ 11-09-23/My1.py
@@ -0,0 +1,13 @@
+# Task1 :Self Intro
+Name="Charles"
+Add="vedhamaharishi nagar,Collector office,Namakkal "
+Class="Msc Computer science"
+T=70
+E=80
+M=79
+P=75
+C=85
+CS=96
+Total=T+E+M+P+C+CS
+Avg=Total/6
+print("My Name is",Name,"From",Add,"Iam doing :",Class,"My Subject Marks are:",T,E,M,P,C,CS,"My Total Marks are:",Total,"My Average is:",Avg)
